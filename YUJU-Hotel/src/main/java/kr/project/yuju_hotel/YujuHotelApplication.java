8f664e2d694936fc6c102d620dfc2b85a100d5e0
jinju0323 <tlawlswn28@naver.com>
2025-02-10 18:54:39 +0900

    0210 Feat: 프로젝트 초기 설정 및 기본 구조 추가

---
+++ YUJU-Hotel/src/main/java/kr/project/yuju_hotel/YujuHotelApplication.java
@@ -0,0 +1,13 @@
+package kr.project.yuju_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YujuHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YujuHotelApplication.class, args);
+	}
+
+}
---
+++ YUJU-Hotel/src/test/java/kr/project/yuju_hotel/YujuHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.project.yuju_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YujuHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
