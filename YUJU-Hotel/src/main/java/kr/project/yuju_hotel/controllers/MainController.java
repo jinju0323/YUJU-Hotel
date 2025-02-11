04d9f7714ec1a4c0758b799a6d9c47f9e9ab4862
jinju0323 <tlawlswn28@naver.com>
2025-02-11 12:56:42 +0900

    0211 Feat: MainContoller 추가 - index.html 테스트

---
+++ YUJU-Hotel/src/main/java/kr/project/yuju_hotel/controllers/MainController.java
@@ -0,0 +1,14 @@
+package kr.project.yuju_hotel.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+    
+}
