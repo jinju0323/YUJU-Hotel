33b0693c449ef8427666eb2baebddde44d9d2146
yujeong <yujeong010817@gmail.com>
2025-02-19 16:07:06 +0900

    0219 Feat: header,footer 너비조정

---
+++ yuju/yuju/src/main/java/kr/project/yuju/controllers/adminController.java
@@ -0,0 +1,13 @@
+package kr.project.yuju.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class adminController {
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "sales";
+    }
+}
